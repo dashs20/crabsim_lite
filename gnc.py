fab1d0fe14acc27c539e353f9c341d82b00fc53f
Dash <143964362+dashs20@users.noreply.github.com>
2026-05-23 22:05:30 -0700

    gnc sim runs
    who knows if it's right?!

--- gnc.py
+++ gnc.py
@@ -15,13 +15,13 @@ class allocator:
         rnx_z = r_cg2nx_m[2]
 
         # build actuator to moment matrix
-        act_2_M = np.array([[-rpx_z,0,-rnx_z,0],
-                            [0,-rpx_x,0,-rnx_x],
-                            [rpx_x,0,rnx_x,0],
-                            [0,1,0,1]])
+        self.act_2_M = np.array([[-rpx_z,0,-rnx_z,0],
+                                 [0,-rpx_x,0,-rnx_x],
+                                 [rpx_x,0,rnx_x,0],
+                                 [0,1,0,1]])
         
         # take inverse (only need to do this once! More performant)
-        self.M_2_act = np.linalg.inv(act_2_M)
+        self.M_2_act = np.linalg.inv(self.act_2_M)
 
         # force to rotor rate lookup
         raw_data = np.loadtxt(motor_data, delimiter=',', skiprows=1)
@@ -34,15 +34,15 @@ class allocator:
     def allocate(self,M_desired_Nm,thr_frac):
 
         # clip throttle
-        thr_frac = np.min(thr_frac,self.min_thr_frac)
+        thr_frac = min(thr_frac,self.min_thr_frac)
 
         # build desired actuator "ask"
         Fz_desired_N = thr_frac * -2 * self.f_max_N # 2 thrusters, -z is up
-        ask = np.vstack((M_desired_Nm,
+        ask = np.hstack((M_desired_Nm,
                          Fz_desired_N))
 
         # obtain actuator forces
-        f = self.M_2_act @ ask
+        f = self.M_2_act @ np.transpose(ask)
 
         # contents of f:
         # [0]: fpx_y
@@ -65,7 +65,7 @@ class allocator:
     # parity is crucial. Without these tables, the controller can freely command moments the plant
     # cannot achieve.
     def compute_max_authority(self):
-        
+
         # local helper functions
         def compute_F_N(f_N,phi_rad):
             T_hat = compute_T_hat(phi_rad)
@@ -75,28 +75,31 @@ class allocator:
             Fpx = compute_F_N(self.f_max_N*thr_frac,self.phi_max_rad)
             Fnx = Fpx
 
-            Mx_max_Nm = self.M_2_act @ np.array([[Fpx[1]],
-                                                 [Fpx[2]],
-                                                 [Fnx[1]],
-                                                 [Fnx[2]]])
+            Mx_max_Nm = self.act_2_M @ np.array([Fpx[1],
+                                                 Fpx[2],
+                                                 Fnx[1],
+                                                 Fnx[2]])
+            return Mx_max_Nm[0,0]
         
         def get_max_y(self,thr_frac):
             Fpx = compute_F_N(self.f_max_N*thr_frac,0)
             Fnx = np.zeros((3,1))
 
-            My_max_Nm = self.M_2_act @ np.array([[Fpx[1]],
-                                                 [Fpx[2]],
-                                                 [Fnx[1]],
-                                                 [Fnx[2]]])
+            My_max_Nm = self.act_2_M @ np.array([Fpx[1],
+                                                 Fpx[2],
+                                                 Fnx[1],
+                                                 Fnx[2]])
+            return My_max_Nm[1,0]
         
         def get_max_z(self,thr_frac):
             Fpx = compute_F_N(self.f_max_N*thr_frac,self.phi_max_rad)
             Fnx = compute_F_N(self.f_max_N*thr_frac,-self.phi_max_rad)
 
-            Mz_max_Nm = self.M_2_act @ np.array([[Fpx[1]],
-                                                [Fpx[2]],
-                                                [Fnx[1]],
-                                                [Fnx[2]]])
+            Mz_max_Nm = self.act_2_M @ np.array([Fpx[1],
+                                                 Fpx[2],
+                                                 Fnx[1],
+                                                 Fnx[2]])
+            return Mz_max_Nm[2,0]
         
         # perform a sweep to generate max moments as a function of throttle
         n_checks = 100
@@ -107,26 +110,97 @@ class allocator:
         Mz_max_Nm = np.zeros(np.size(thr_fracs))
 
         for i in range(n_checks):
-            Mx_max_Nm[i] = get_max_x(thr_fracs[i])
-            My_max_Nm[i] = get_max_y(thr_fracs[i])
-            Mz_max_Nm[i] = get_max_z(thr_fracs[i])
+            Mx_max_Nm[i] = get_max_x(self,thr_fracs[i])
+            My_max_Nm[i] = get_max_y(self,thr_fracs[i])
+            Mz_max_Nm[i] = get_max_z(self,thr_fracs[i])
 
         # build lookups
         thr_frac_2_max_Mx = lookup_1D(thr_fracs,Mx_max_Nm)
         thr_frac_2_max_My = lookup_1D(thr_fracs,My_max_Nm)
         thr_frac_2_max_Mz = lookup_1D(thr_fracs,Mz_max_Nm)
 
-        return thr_frac_2_max_Mx, thr_frac_2_max_My, thr_frac_2_max_Mz
+        return [thr_frac_2_max_Mx, thr_frac_2_max_My, thr_frac_2_max_Mz]
 
-class GNC:
-    def __init__(self,yaml_path):
+class PID:
+    def __init__(self,gains,i_lims,dt_s):
+        self.dt_s = dt_s
+        self.kp = gains[0]
+        self.ki = gains[1]
+        self.kd = gains[2]
+        self.i_lims = i_lims
+        self.i_err = 0
+        self.prev_err = 0
+
+    def get_cmd(self,des_state,cur_state):
+        # compute error
+        err =  des_state - cur_state
+
+        # compute the derivative of the error
+        d_err = (err - self.prev_err) / self.dt_s
+
+        # update the integral of the error
+        self.i_err += self.dt_s * err
+        self.i_err = np.clip(self.i_err, self.i_lims[0], self.i_lims[1])
+
+        # save previous error
+        self.prev_err = err
+
+        # compute output signal
+        return self.kp * err + self.ki * self.i_err + self.kd * d_err
+
+class smart_PID:
+    def __init__(self,yaml_path,M_max_lookups_Nm,dt_s):
+        # open yaml
+        with open(yaml_path, 'r') as file:
+            data = yaml.safe_load(file)
+        
+        self.dt_s = dt_s
+        self.PID_x = PID(data['x_gains'],data['x_i_lim'] * np.array([-1,1]),self.dt_s)
+        self.PID_y = PID(data['y_gains'],data['y_i_lim'] * np.array([-1,1]),self.dt_s)
+        self.PID_z = PID(data['z_gains'],data['z_i_lim'] * np.array([-1,1]),self.dt_s)
+
+        self.Mx_max_lookup_Nm = M_max_lookups_Nm[0]
+        self.My_max_lookup_Nm = M_max_lookups_Nm[1]
+        self.Mz_max_lookup_Nm = M_max_lookups_Nm[2]
+
+    def get_M_cmd(self,w_des_radps,w_est_radps,thr_frac):
+        Mx_cmd = min(self.Mx_max_lookup_Nm.index(thr_frac),self.PID_x.get_cmd(w_des_radps[0],w_est_radps[0,0]))
+        My_cmd = min(self.My_max_lookup_Nm.index(thr_frac),self.PID_y.get_cmd(w_des_radps[1],w_est_radps[1,0]))
+        Mz_cmd = min(self.Mz_max_lookup_Nm.index(thr_frac),self.PID_z.get_cmd(w_des_radps[2],w_est_radps[2,0]))
+
+        return np.array([Mx_cmd,My_cmd,Mz_cmd])
+
+class gnc:
+    def __init__(self,yaml_path,dt_s):
+
+        self.dt_s = dt_s
+
         # open yaml
         with open(yaml_path, 'r') as file:
             data = yaml.safe_load(file)
 
         # build allocator
-        self.allocator = allocator(mm2m(data['r_cg2px_mm']),
-                                   mm2m(data['r_cg2nx_mm']),
+        self.allocator = allocator(mm2m(np.array(data['r_cg2px_mm'])),
+                                   mm2m(np.array(data['r_cg2nx_mm'])),
                                    data['motor_lookup'],
                                    data['min_thr_frac'],
                                    np.deg2rad(data['phi_max_deg']))
+        
+        # build controller
+        self.controller = smart_PID(data['controller_yaml'],
+                                    self.allocator.compute_max_authority(),
+                                    self.dt_s)
+        
+    def step(self,w_des_radps,w_est_radps,thr_frac):
+
+        # run core control algorithm
+        M_cmd_Nm = self.controller.get_M_cmd(w_des_radps,w_est_radps,thr_frac)
+        act_cmd = self.allocator.allocate(M_cmd_Nm,thr_frac)
+
+        # return software state
+        state = np.hstack((M_cmd_Nm,
+                           act_cmd))
+        
+        return state
+
+
---
+++ sim_cl.py
@@ -0,0 +1,61 @@
+from plants import *
+from gnc import *
+from util import *
+
+# define sim dt (seconds)
+dt_s = 1/4000
+t_end_s = 10
+n_steps = round(t_end_s/dt_s)
+t_s = np.linspace(0,t_end_s,n_steps)
+
+# define bicopter
+crabcopter = bicopter('crabcopter.yaml',dt_s)
+
+# define gnc
+crabbrain = gnc('crabcopter_gnc.yaml',dt_s)
+
+# define some controller command lookups
+wx_cmd_lookup_radps = lookup_1D([-dt_s,t_end_s],np.deg2rad([0,30]))
+wy_cmd_lookup_radps = lookup_1D([-dt_s,t_end_s],np.deg2rad([0,-10]))
+wz_cmd_lookup_radps = lookup_1D([-dt_s,t_end_s],np.deg2rad([0,15]))
+thr_lookup_frac = lookup_1D([-dt_s,t_end_s],[0.4,0.4])
+
+# define plant log array
+plant_log_array = np.zeros((n_steps,17))
+plant_log_col_names = ["t_s",
+                        "wx_radps","wy_radps","wz_radps",
+                        "phi_px_rad","phi_nx_rad","phidot_px_radps","phidot_nx_radps",
+                        "w_px_radps,w_nx_radps,wdot_px_radps2,wdot_nx_radps",
+                        "F_px_x_N","F_px_y_N","F_px_z_N",
+                        "F_nx_x_N","F_nx_y_N","F_nx_z_N"]
+plant_log_col_names = ','.join(plant_log_col_names)
+
+# define GNC log array
+gnc_log_array = np.zeros((n_steps,7))
+gnc_log_col_names = ["fpx_frac","fnx_frac","phi_px_rad","phi_nx_rad",
+                     "Mx_cmd_Nm","My_cmd_Nm","Mz_cmd_Nm"]
+gnc_log_col_names = ','.join(gnc_log_col_names)
+
+# perform simulation
+for i_step in range(n_steps):
+
+    t_cur_s = t_s[i_step]
+
+    # get controller commands
+    wx_cmd_radps = wx_cmd_lookup_radps.index(t_cur_s)
+    wy_cmd_radps = wy_cmd_lookup_radps.index(t_cur_s)
+    wz_cmd_radps = wz_cmd_lookup_radps.index(t_cur_s)
+    w_des_radps = np.array([wx_cmd_radps,wy_cmd_radps,wz_cmd_radps])
+    thr_frac = thr_lookup_frac.index(t_cur_s)
+
+    # step GNC & log outputs
+    gnc_log_array[i_step,:] = crabbrain.step(w_des_radps,crabcopter.body.W_B_wrt_I_radps,thr_frac)
+    act_cmd = gnc_log_array[i_step,0:4]
+
+    # step plant & log output
+    plant_log_array[i_step,:] = crabcopter.step(act_cmd)
+
+# build composite log
+log_array = np.hstack((t_s[:,np.newaxis],plant_log_array,gnc_log_array))
+log_col_names = "t_s" + plant_log_col_names + gnc_log_col_names
+np.savetxt('log.csv', log_array, delimiter=',', fmt='%.3f', header=log_col_names, comments='')
