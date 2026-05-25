59ad45bfd176ddc40338aed77f0a4fccddbd47e9
Dash <143964362+dashs20@users.noreply.github.com>
2026-05-25 09:52:46 -0700

    Update sympy_plant.py

--- sympy_plant.py
+++ sympy_plant.py
@@ -56,3 +56,53 @@ Net moment
 """
 
 M_net = M_thrust + M_wheel_px + M_wheel_nx
+
+sp.pprint(M_net)
+
+# This equation is the problem and the solution. It maps the actuator and vehicle state directly to the moment on the vehicle.
+# The million dollar question: how can we get control inputs u such that this equation equals a desired moment M?
+# The thing is, this is a differential equation, not a normal equation. It contains derivatives with respect to time of the actuators,
+# as well as their current state. This equation cannot be solved instantaneously; we cannot make the actuators do whatever we want.
+# The actuators have their own lag; their own physics. But, we know that too. So, we can also get differential equations for the actuator
+# states and state derivatives as a function of U. Let's do it.
+
+"""
+Transient parameters
+"""
+
+tau_r = sp.symbols('tau_r') # rotor/motor time constant
+tau_s = sp.symbols('tau_s') # servo time constant
+k_r, k_s = sp.symbols('k_r k_s') # DC gains
+
+"""
+Inputs
+"""
+
+phi_px_cmd, phi_nx_cmd = sp.symbols('phi_px_cmd phi_nx_cmd')
+omega_rpx_cmd, omega_rnx_cmd = sp.symbols('omega_rpx_cmd omega_rnx_cmd')
+
+"""
+Outputs
+"""
+
+phidot_px = -1 / tau_s * phi_px + k_s / tau_s * phi_px_cmd
+phidot_nx = -1 / tau_s * phi_nx + k_s / tau_s * phi_nx_cmd
+omegadot_rpx = -1 / tau_r * omega_rpx + k_r / tau_r * omega_rpx_cmd
+omegadot_rnx = -1 / tau_r * omega_rnx + k_r / tau_r * omega_rnx_cmd
+
+sp.pprint([phidot_px,phidot_nx,omegadot_rpx,omegadot_rnx])
+
+# now we have the whole story. We have
+# - 3 differential equations describing the moment on the body as a function of the vehicle state and its derivatives
+# - 4 differential equations mapping control input u to rate change of the vehicle states
+
+# here's an idea. we have an M_cmd. we want it to go TOWARDS M_actual.
+# 1) for everything we DO NOT have control over (which is just omega_b) plug it in.
+# 2) obtain the derivative of the moment equation with respect to time.
+# 3) zero all terms beyond 1st order.
+# 4) solve for control inputs u that minimize dM/dt towards M_desired.
+
+# alternatively, consider that we have an omega_b_desired and an omega_b_current. we want to drive omega_b towards the desired state.
+# to that end, we wish to maximzie omegadot_b towards omega_b. omegadot_b can be approximated as B * M. And M is the big equation we
+# already have.
+
